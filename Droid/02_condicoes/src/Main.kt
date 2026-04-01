1fb4391ccadaa3592311fee1dbab42111cd84f3c
GoncaloaaF <goncaloaaf@gmail.com>
2026-04-01 17:21:06 +0100

    intro a condições

---
+++ Droid/02_condicoes/src/Main.kt
@@ -0,0 +1,71 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+    val altura_float: Float = 0f
+    val altura_Double: Double = 0.0
+
+
+    var idade = 31
+
+   // idade = IO.readln("Idade: ").toInt()
+
+    if (idade >= 18) {
+        println("Adulto")
+    }else if (idade > 12) {
+        println("Teen")
+    }else{
+        println("Criança")
+    }
+
+    // var mes = IO.readln("mes: ").toInt()
+
+    var mes = 10
+    when (mes){
+        1 -> println("Jan")
+        2 -> println("Feb")
+        3 -> println("Mar")
+        4 -> println("Abril")
+        5 -> println("May")
+        6 -> println("Jun")
+        7 -> println("Jul")
+        8 -> println("Aug")
+        9 -> println("Sep")
+        10 -> println("Oct")
+        11 -> println("Nov")
+        12 -> println("Dec")
+        else -> println("Mês invalido")
+    }
+
+
+ //   var mes2 = IO.readln("mes2: ").toInt()
+    val mes2 = 12
+    val resp = when (mes2) {
+        1 -> "Jan"
+        2 -> "Feb"
+        3 -> "Mar"
+        4 -> "Abril"
+        5 -> "May"
+        6 -> "Jun"
+        7 -> "Jul"
+        8 -> "Aug"
+        9 -> "Sep"
+        10 -> "Oct"
+        11 -> "Nov"
+        12 -> "Dec"
+        else -> "Unknown"
+    }
+
+    println(resp)
+
+    val num = 5
+    when (num) {
+        1,2,3,6,9 -> println("Bloco 1")
+        5,4,7 -> println("Bloco 2")
+        else -> println("Bloco 3")
+    }
+
+
+
+
+}
